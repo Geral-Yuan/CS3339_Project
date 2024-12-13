3ea31c09edce33f260df49bb832d9af45bb9b810
Geral-Yuan <yjl2030906598@sjtu.edu.cn>
2024-12-13 13:30:37 +0800

    add SMOTE and data preprocess

--- main.py
+++ main.py
@@ -24,17 +24,58 @@ def main(args):
     with open('data/train_feature.pkl', 'rb') as f:
         X = pickle.load(f).toarray()
     
-    
-    # X = np.sqrt(X) # 0.92134
-    # X = X ** 0.25 # 0.91975
-    X = np.sqrt(1-(1-X)**2) # 0.92178
-    # X = np.log1p(X*np.e) # 0.91877
-    # X = np.tanh(X)
-    
     y = np.load('data/train_labels.npy')
+    
+    # default 0.91223
+    
+    # X = X[:, X.max(axis=0) > 1e-6]
+
+    X = np.sqrt(X) # 0.92125 | 0.92842
+    # X = X ** 0.25 # 0.92143 | 0.92858
+    # X = np.sqrt(1-(1-X)**2) # 0.92213 | 0.92842
+    # X[X > 1e-6] = -1/np.log(X[X > 1e-6]) # 0.91957
+    
+    # offset = 0
+    # scale = 1
+    # N = 40
+    # for i in range(N):
+    #     X[(X > i/N)&(X < (i+1)/N)] = (X[(X > i/N)&(X < (i+1)/N)] - i/N) * scale + offset
+    #     offset += scale / N
+    #     scale -= 1 / N
+        
+    # X = np.log(X+3e-3)
+    # X -= X.min(axis=0)
+    # X /= X.max(axis=0)
+    
+    # X = np.log1p(X) # 0.91302
+    # X = np.log1p(X*np.e) # 0.91904
+    
+    # import warnings
+    # warnings.filterwarnings("ignore", category=FutureWarning)
+    
+    # from imblearn.over_sampling import BorderlineSMOTE
+    # borderline_smote = BorderlineSMOTE(random_state=42)
+    # print("Original data shape:", X.shape, y.shape)
+    # X, y = borderline_smote.fit_resample(X, y)
+    # print("BorderlineSMOTE data shape:", X.shape, y.shape)
+    
+    # from imblearn.over_sampling import SMOTE
+    # print("Original data shape:", X.shape, y.shape)
+    # X, y = SMOTE(random_state=42).fit_resample(X, y)
+    # print("SMOTE data shape:", X.shape, y.shape)
+    
+    # for i in range(3):
+    #     if i == 0:
+    #         X = np.sqrt(X)
+    #     elif i == 1:
+    #         X = X ** 0.25
+    #     elif i == 2:
+    #         X = np.sqrt(1-(1-X)**2)
+    #     train_val(args, X, y, i)
+    #     with open('data/train_feature.pkl', 'rb') as f:
+    #         X = pickle.load(f).toarray()
 
     train_val(args, X, y)
-    
 
 if __name__ == '__main__':
     timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
@@ -49,7 +90,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     log_file = open(args.log_file, "a")
     sys.stdout = Tee(log_file)
-    print("-"*50)
+    print("-"*100)
     print(f"{timestamp} run with arguments: {args}")
     main(args)
     sys.stdout = sys.__stdout__
--- validation.py
+++ validation.py
@@ -26,6 +26,20 @@ def cross_val(args, X, y, param, k_folds=5):
     for fold, (train_idx, val_idx) in enumerate(kfold.split(X)):
         X_train, X_val = X[train_idx], X[val_idx]
         y_train, y_val = y[train_idx], y[val_idx]
+
+        # import warnings
+        # warnings.filterwarnings("ignore", category=FutureWarning)
+        
+        # from imblearn.over_sampling import BorderlineSMOTE
+        # borderline_smote = BorderlineSMOTE(random_state=42)
+        # print("Original data shape:", X_train.shape, y_train.shape)
+        # X_train, y_train = borderline_smote.fit_resample(X_train, y_train)
+        # print("BorderlineSMOTE data shape:", X_train.shape, y_train.shape)
+        
+        # from imblearn.over_sampling import SMOTE
+        # print("Original data shape:", X_train.shape, y_train.shape)
+        # X_train, y_train = SMOTE().fit_resample(X_train, y_train)
+        # print("SMOTE data shape:", X_train.shape, y_train.shape)
         
         if args.use_pca:
             if args.load_pca:
@@ -74,32 +88,32 @@ def cross_val(args, X, y, param, k_folds=5):
         
         
 
-def grid_search(args, X, y, params):
+def grid_search(args, X, y, params, id):
     keys = list(params.keys())
     values = [params[key] if isinstance(params[key], list) else [params[key]] for key in keys]
     
     best_params = None
     best_score = 0
-    results_df= pd.DataFrame(columns=list(params.keys())+['train_accuracy', 'val_accuracy'])
+    results_df= pd.DataFrame(columns=list(params.keys())+['train_accuracy', 'val_accuracy', 'func_id'])
     for combination in product(*values):
         param_combination = dict(zip(keys, combination))
         train_acc, val_acc = cross_val(args, X, y, param_combination)
         if val_acc > best_score:
             best_score = val_acc
             best_params = param_combination
-        new_row = pd.Series({**param_combination, 'train_accuracy': train_acc, 'val_accuracy': val_acc})
+        new_row = pd.Series({**param_combination, 'train_accuracy': train_acc, 'val_accuracy': val_acc, 'func_id': id})
         if results_df.empty:
             results_df = new_row.to_frame().T
         else:
             results_df = pd.concat([results_df, new_row.to_frame().T], ignore_index=True)
 
-    results_df.to_csv(f"results/{args.result_file}", index=False)
+    results_df.to_csv(f"results/{id}_{args.result_file}", index=False)
     print(f"Best validation accuracy: {best_score}, Best params: {best_params}")
         
 
         
 
-def train_val(args, X, y):
+def train_val(args, X, y, id=0):
     if args.model == 'RndFrst':
         pass
         # model = RandomForestClassifier(n_estimators=100, max_features='sqrt', random_state=42)
@@ -119,41 +133,31 @@ def train_val(args, X, y):
         pass
         # model = KNeighborsClassifier(n_neighbors=15)
     elif args.model == 'MLP':
-        ## running
         # params = {
-        #     'hidden_size': [256, 256+128, 512, 512+128, 512+256],
-        #     'drop_rate': [0.8, 0.85, 0.9],
-        #     'weight_decay': [1e-7, 5e-7, 1e-6],
+        #     'hidden_size': [384, 512, 640],
+        #     'drop_rate': [0.9, 0.95, 0.98],
+        #     'weight_decay': [3e-7, 1e-7, 3e-8],
         #     'lr': [1e-3],
-        #     'batch_size': [32, 64, 128],
-        #     'epoch_num': [20, 25],
-        #     'mask_prob': [0.00]
+        #     'batch_size': [256],
+        #     'epoch_num': [40, 50],
+        #     'mask_prob': [0]
         # }
         # params = {
-        #     'hidden_size': [512, 512+256, 1024],
-        #     'drop_rate': [0.8, 0.85, 0.9],
-        #     'weight_decay': [1e-7, 1e-6],
-        #     'lr': [1e-3, 5e-4, 1e-4],
-        #     'batch_size': [64, 128, 256],
-        #     'epoch_num': [15, 20],
-        #     'mask_prob': [0.00, 0.01, 0.02]
-        # }
-        # params = {
-        #     'hidden_size': [384, 512],
+        #     'hidden_size': [512, 1024],
         #     'drop_rate': 0.9,
-        #     'weight_decay': 1e-7,
-        #     'lr': 1e-3,
-        #     'batch_size': [16, 32],
-        #     'epoch_num': 25,
+        #     'weight_decay': [3e-7, 1e-7],
+        #     'lr': [1e-3, 5e-4],
+        #     'batch_size': [128, 256],
+        #     'epoch_num': [30, 50, 100, 200],
         #     'mask_prob': 0
         # }
         params = {
-            'hidden_size': 384,
-            'drop_rate': 0.9,
-            'weight_decay': 5e-7,
+            'hidden_size': 640,
+            'drop_rate': 0.95,
+            'weight_decay': 3e-8,
             'lr': 1e-3,
-            'batch_size': 32,
-            'epoch_num': 25,
+            'batch_size': 256,
+            'epoch_num': 50,
             'mask_prob': 0
         }
         
@@ -162,6 +166,6 @@ def train_val(args, X, y):
         raise ValueError('model not supported')
         
     start_time = time.time()
-    grid_search(args, X, y, params)
+    grid_search(args, X, y, params, id)
     end_time = time.time()
     print(f"Total grid search time: {end_time - start_time:.2f}s")
