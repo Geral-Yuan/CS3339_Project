c6ef393a7a73c278b795cb62adfd32998967a823
Geral-Yuan <yjl2030906598@sjtu.edu.cn>
2024-12-14 22:20:47 +0800

    add sqrt to single hidden layer MLP and design two hidden layers MLP with residual connection

--- model/mlp.py
+++ model/mlp.py
@@ -18,13 +18,37 @@ class MLP(nn.Module):
         x = self.fc1(x)
         x = self.dropout(x)
         x = torch.relu(x)
+        x = torch.sqrt(x)
+        x = self.fc2(x)
+        return x
+    
+class resNetMLP(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size=128, drop_rate=0.5):
+        super(resNetMLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.resNet = nn.Linear(hidden_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(drop_rate)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = torch.relu(x)
+        # x = torch.sqrt(x)
+        x = self.resNet(x) + x
+        x = self.dropout(x)
+        x = torch.relu(x)
+        # x = torch.sqrt(x)
         x = self.fc2(x)
         return x
 
-class MLPClassifier(ClassifierMixin, BaseEstimator):
-    def __init__(self, input_size, output_size, hidden_size=128, drop_rate=0.5, weight_decay=0.0, lr=0.001, batch_size=128, epoch_num=10, mask_prob=0.2):
+class MLPClassifier(ClassifierMixin, BaseEstimator, nn.Module):
+    def __init__(self, input_size, output_size, resNet=False, hidden_size=128, drop_rate=0.5, weight_decay=0.0, lr=0.001, batch_size=128, epoch_num=10, mask_prob=0.2):
         super(MLPClassifier, self).__init__()
-        self.model = MLP(input_size, output_size, hidden_size, drop_rate)
+        if resNet:
+            self.model = resNetMLP(input_size, output_size, hidden_size, drop_rate)
+        else:
+            self.model = MLP(input_size, output_size, hidden_size, drop_rate)
         self.weight_decay = weight_decay
         self.lr = lr
         self.batch_size = batch_size
@@ -34,6 +58,9 @@ class MLPClassifier(ClassifierMixin, BaseEstimator):
     def fit(self, train_feature, train_label):
         optimizer = Adam(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         criterion = nn.CrossEntropyLoss()
+        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.3)
+        # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50], gamma=0.5)
+        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50, eta_min=0.01)
 
         train_feature = torch.tensor(train_feature, dtype=torch.float32).cuda()
         train_label = torch.tensor(train_label, dtype=torch.long).cuda()
@@ -60,6 +87,7 @@ class MLPClassifier(ClassifierMixin, BaseEstimator):
                 optimizer.step()
             if self.mask_prob > epsilon:
                 dataset.refresh_mask()
+            # scheduler.step()
 
     def predict(self, test_features):
         test_features = torch.tensor(test_features, dtype=torch.float32).cuda()
