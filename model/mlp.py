d177a99dd78b2f8fa3ef4a36a07702313a455224
Geral-Yuan <yjl2030906598@sjtu.edu.cn>
2024-12-24 20:25:53 +0800

    make hidden layer of MLP extractable and use CrossEntropyLoss with weights

--- model/mlp.py
+++ model/mlp.py
@@ -22,6 +22,13 @@ class MLP(nn.Module):
         x = self.fc2(x)
         return x
     
+    def hidden_layer(self, x):
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = torch.relu(x)
+        x = torch.sqrt(x)
+        return x
+    
 class resNetMLP(nn.Module):
     def __init__(self, input_size, output_size, hidden_size=128, drop_rate=0.5):
         super(resNetMLP, self).__init__()
@@ -56,8 +63,16 @@ class MLPClassifier(ClassifierMixin, BaseEstimator, nn.Module):
         self.mask_prob = mask_prob
 
     def fit(self, train_feature, train_label):
+        # total_cnt = train_label.shape[0]
+        label_cnt = []
+        for i in range(20):
+            label_cnt.append((train_label == i).sum().item())
+        # class_weights = [total_cnt / label_cnt[i] for i in range(20)]
+        # class_weights = [1/torch.log1p(torch.tensor(label_cnt[i], dtype=torch.float32).cuda()) for i in range(20)]
+        class_weights = [1/torch.sqrt(torch.tensor(label_cnt[i])) for i in range(20)]
+        
         optimizer = Adam(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss(weight=torch.tensor(class_weights, dtype=torch.float32).cuda())
         # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.3)
         # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50], gamma=0.5)
         # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50, eta_min=0.01)
@@ -105,3 +120,11 @@ class MLPClassifier(ClassifierMixin, BaseEstimator, nn.Module):
         predicted_labels = self.predict(test_features)
         accuracy = (predicted_labels == test_labels).mean()
         return accuracy
+    
+    def hidden_layer(self, X):
+        X = torch.tensor(X, dtype=torch.float32).cuda()
+        self.model = self.model.cuda()
+        self.model.eval()
+        with torch.no_grad():
+            hidden = self.model.hidden_layer(X)
+        return hidden.cpu().numpy()
